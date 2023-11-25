ffa8cdeae8c23ed6f873decae0a146cafb65fec5
albinz222 <119280618+albinz222@users.noreply.github.com>
2023-11-25 10:45:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/albinz222/AutoFilterAdvance/AutoFilterAdvance
+  git clone https://github.com/albinz222/AutoFilterAdvance
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /AutoFilterAdvance
